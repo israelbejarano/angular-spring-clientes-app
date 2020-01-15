075feb2f8a8a40a118295989b0345cc0e241f93f
israel <israel.bejarano.a@gmail.com>
2020-01-15 16:24:54 +0100

    componente directiva (for-if)

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { DirectivaComponent } from './directiva/directiva.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    DirectivaComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    DirectivaComponent
   ]
 })
 export class ComponentsModule { }
