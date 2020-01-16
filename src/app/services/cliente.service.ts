bc2a95f4d146b73a62df9866cae09ee0b323ba18
israel <israel.bejarano.a@gmail.com>
2020-01-16 20:39:29 +0100

    traer los datos del backend java

--- src/app/services/cliente.service.ts
+++ src/app/services/cliente.service.ts
@@ -2,15 +2,19 @@ import { Injectable } from '@angular/core';
 import { Cliente } from '../interfaces/cliente';
 import { CLIENTES } from '../components/clientes/clientes.json';
 import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:8080/api/';
+
+  constructor(private http: HttpClient) { }
 
   public getClientes(): Observable<Cliente[]> {
-    return of(CLIENTES);
+    // return of(CLIENTES);
+    return this.http.get<Cliente[]>(this.baseUrl + 'clientes');
   }
 }
