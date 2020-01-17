6f88fa1c45a82fcf5f58c73e508cc4580168bd2e
israel <israel.bejarano.a@gmail.com>
2020-01-17 12:20:31 +0100

    ruta para el form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes, RouterModule } from '@angular/router';
 import { DirectivaComponent } from './components/directiva/directiva.component';
 import { ClientesComponent } from './components/clientes/clientes.component';
+import { FormComponent } from './components/clientes/form.component';
 
 const appRoutes: Routes = [
     {path: '', redirectTo: '/clientes', pathMatch: 'full'},
     {path: 'directivas', component: DirectivaComponent},
     {path: 'clientes', component: ClientesComponent},
+    {path: 'clientes/form', component: FormComponent}
 ];
 
 
