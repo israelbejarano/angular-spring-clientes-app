9e0aeb9aac4fae5b70dbf3e929dcf3342ac6935d
israel <israel.bejarano.a@gmail.com>
2020-01-17 13:27:07 +0100

    uso de sweet alert + redireccion a clientes

--- src/app/components/clientes/form.component.ts
+++ src/app/components/clientes/form.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Cliente } from '../../interfaces/cliente';
 import { ClienteService } from '../../services/cliente.service';
 
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html'
@@ -11,15 +14,21 @@ export class FormComponent implements OnInit {
   cliente: Cliente = new Cliente();
   titulo = 'Crear Cliente';
 
-  constructor(private clienteService: ClienteService) { }
+  constructor(private clienteService: ClienteService, private router: Router) { }
 
   ngOnInit() {
   }
 
   public create(): void {
-    console.log(this.cliente);
+    // console.log(this.cliente);
     this.clienteService.create(this.cliente).subscribe((clienteResp: Cliente) => {
-      console.log(clienteResp);
+      Swal.fire({
+        icon: 'success',
+        title: `Cliente ${clienteResp.nombre} ${clienteResp.apellido} creado con éxito`,
+        showConfirmButton: false,
+        timer: 1500
+      });
+      this.router.navigate(['/clientes']);
     });
   }
 
