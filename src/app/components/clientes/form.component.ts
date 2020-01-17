dfe826119db0cbe050ad89dabe50ce22f887ed9d
israel <israel.bejarano.a@gmail.com>
2020-01-17 12:18:09 +0100

    formulario nuevo cliente

---
+++ src/app/components/clientes/form.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Cliente } from '../../interfaces/cliente';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html'
+})
+export class FormComponent implements OnInit {
+
+  cliente: Cliente;
+  titulo = 'Crear Cliente';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  public create(): void {
+    console.log(this.cliente);
+  }
+
+}
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { DirectivaComponent } from './directiva/directiva.component';
 import { ClientesComponent } from './clientes/clientes.component';
+import { FormComponent } from './clientes/form.component';
 
 
 
@@ -13,11 +15,13 @@ import { ClientesComponent } from './clientes/clientes.component';
     HeaderComponent,
     FooterComponent,
     DirectivaComponent,
-    ClientesComponent
+    ClientesComponent,
+    FormComponent
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    FormsModule
   ],
   exports: [
     HeaderComponent,
