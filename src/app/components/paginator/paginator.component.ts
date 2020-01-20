248acfd63667cecae267e6f5221ac74ba15f1f1f
israel <israel.bejarano.a@gmail.com>
2020-01-20 16:48:09 +0100

    overflow para el paginador

--- src/app/components/paginator/paginator.component.ts
+++ src/app/components/paginator/paginator.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { ClientePaginadoResponse } from '../../interfaces/api';
 
 @Component({
@@ -6,16 +6,36 @@ import { ClientePaginadoResponse } from '../../interfaces/api';
   templateUrl: './paginator.component.html',
   styleUrls: ['./paginator.component.css']
 })
-export class PaginatorComponent implements OnInit {
+export class PaginatorComponent implements OnInit, OnChanges {
 
   @Input() paginador: ClientePaginadoResponse;
   paginas: number[];
+  desde: number;
+  hasta: number;
 
   constructor() { }
 
   ngOnInit() {
-    // tslint:disable-next-line: variable-name
-    this.paginas = new Array(this.paginador.totalPages).fill(0).map((_valor, indice) => indice + 1);
+    this.initPaginator();
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    const paginadorActualizado = changes['paginador'.toString()];
+    if (paginadorActualizado.previousValue) {
+      this.initPaginator();
+    }
+  }
+
+  private initPaginator() {
+    this.desde = Math.min(Math.max(1, this.paginador.number - 4), this.paginador.totalPages - 5);
+    this.hasta = Math.max(Math.min(this.paginador.totalPages, this.paginador.number + 4), 6);
+    if (this.paginador.totalPages > 5) {
+      // tslint:disable-next-line: variable-name
+      this.paginas = new Array(this.hasta - this.desde + 1).fill(0).map((_valor, indice) => indice + this.desde);
+    } else {
+      // tslint:disable-next-line: variable-name
+      this.paginas = new Array(this.paginador.totalPages).fill(0).map((_valor, indice) => indice + 1);
+    }
   }
 
 }
