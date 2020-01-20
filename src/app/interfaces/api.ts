b1f0d2970211ddf75ea1d4525e45421b412cb5e4
israel <israel.bejarano.a@gmail.com>
2020-01-20 15:40:35 +0100

    interfaz para clientes paginados

--- src/app/interfaces/api.ts
+++ src/app/interfaces/api.ts
@@ -1,2 +1,3 @@
 export * from './cliente';
 export * from './cliente.response';
+export * from './cliente.paginado.response';
---
+++ src/app/interfaces/cliente.paginado.response.ts
@@ -0,0 +1,31 @@
+import { Cliente } from './cliente';
+
+export interface ClientePaginadoResponse {
+    content: Cliente[];
+    pageable: Pageable;
+    last: boolean;
+    totalPages: number;
+    totalElements: number;
+    size: number;
+    number: number;
+    sort: Sort;
+    numberOfElements: number;
+    first: boolean;
+    empty: boolean;
+
+}
+
+interface Pageable {
+    sort: Sort;
+    offset: number;
+    pageNumber: number;
+    pageSize: number;
+    paged: boolean;
+    unpaged: boolean;
+}
+
+interface Sort {
+    sorted: boolean;
+    unsorted: boolean;
+    empty: boolean;
+}
