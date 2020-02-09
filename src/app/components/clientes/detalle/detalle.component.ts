14fcd9654cf6e4cfadf75b19c1925de5e0a32903
israel <israel.bejarano.a@gmail.com>
2020-02-09 12:31:29 +0100

    validando imagenes

--- src/app/components/clientes/detalle/detalle.component.ts
+++ src/app/components/clientes/detalle/detalle.component.ts
@@ -14,7 +14,7 @@ export class DetalleComponent implements OnInit {
 
   titulo = 'Detalle del cliente';
   cliente: Cliente;
-  private fotoSeleccionada: File;
+  fotoSeleccionada: File;
 
   constructor(private clienteService: ClienteService, private activatedRoute: ActivatedRoute) { }
 
@@ -32,16 +32,23 @@ export class DetalleComponent implements OnInit {
   seleccionarFoto(event) {
     this.fotoSeleccionada = event.target.files[0];
     console.log(this.fotoSeleccionada);
+    if (this.fotoSeleccionada.type.indexOf('image') < 0) {
+      Swal.fire('Error seleccionar imagen', 'El archivo debe ser del tipo imagen', 'error');
+      this.fotoSeleccionada = null;
+    }
   }
 
   subirFoto() {
-    this.clienteService.subirfoto(this.fotoSeleccionada, this.cliente.id.toString())
-    .subscribe((response: ClienteResponse) => {
-      console.log(response);
-      this.cliente = response.cliente;
-      Swal.fire('la foto se ha subido correctamente', response.mensaje, 'success');
-
-    });
+    if (!this.fotoSeleccionada) {
+      Swal.fire('Error Upload', 'Debe seleccionar una foto', 'error');
+    } else {
+      this.clienteService.subirfoto(this.fotoSeleccionada, this.cliente.id.toString())
+      .subscribe((response: ClienteResponse) => {
+        console.log(response);
+        this.cliente = response.cliente;
+        Swal.fire('la foto se ha subido correctamente', response.mensaje, 'success');
+      });
+    }
   }
 
 }
