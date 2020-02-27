5cf3301f6633e910ed0d7ed2f390021858190ca0
israel <israel.bejarano.a@gmail.com>
2020-02-27 17:22:36 +0100

    opcion por defecto en el selector

--- src/app/components/clientes/form.component.ts
+++ src/app/components/clientes/form.component.ts
@@ -73,6 +73,11 @@ export class FormComponent implements OnInit {
   }
 
   compararRegion(o1: Region, o2: Region) {
+
+    if (o1 === undefined && o2 === undefined) {
+      return true;
+    }
+
     if (o1 === null || o2 === null || o1 === undefined || o2 === undefined) {
       return false;
     } else if (o1.id === o2.id) {
