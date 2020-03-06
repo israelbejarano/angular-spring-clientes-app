de85a2d9736534c7659d1d7cc53394246de12c7e
israel <israel.bejarano.a@gmail.com>
2020-03-06 20:56:57 +0100

    metodo login

--- src/app/components/usuarios/login.component.ts
+++ src/app/components/usuarios/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from '../../interfaces/api';
+
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -8,10 +11,20 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   titulo = 'Por favor Sign in';
+  usuario: Usuario;
 
-  constructor() { }
+  constructor() {
+    this.usuario = new Usuario();
+   }
 
   ngOnInit() {
+  }
+
+  login() {
+    console.log(this.usuario);
+    if (this.usuario.username == null || this.usuario.password == null) {
+      Swal.fire('Error Login', 'Username o password vacías', 'error');
+    }
   }
 
 }
