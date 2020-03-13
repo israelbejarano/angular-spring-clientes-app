5b6566faa7db40a4c012fcb03b1afb8679beabbe
israel <israel.bejarano.a@gmail.com>
2020-03-13 12:36:08 +0100

    interfaz de la respuesta del login

---
+++ src/app/interfaces/login-response.ts
@@ -0,0 +1,12 @@
+export interface LoginResponse {
+    access_token: string;
+    token_type: string;
+    refresh_token: string;
+    expires_in: number;
+    scope: string;
+    apellido: string;
+    onfo_adicional: string;
+    nombre: string;
+    email: string;
+    jti: string;
+}
