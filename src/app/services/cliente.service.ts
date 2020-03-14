31a86bd90b58a7e605352d4f651716ffb7f7c282
israel <israel.bejarano.a@gmail.com>
2020-03-14 13:43:13 +0100

    Enviando token al backend para acceder a los recursos protegidos

--- src/app/services/cliente.service.ts
+++ src/app/services/cliente.service.ts
@@ -7,6 +7,7 @@ import { catchError, map } from 'rxjs/operators';
 import { Cliente, ClienteResponse, ClientePaginadoResponse, Region } from '../interfaces/api';
 
 import Swal from 'sweetalert2';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -16,7 +17,15 @@ export class ClienteService {
   private baseUrl = 'http://localhost:8080/api';
   private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'});
 
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private http: HttpClient, private router: Router, private authService: AuthService) { }
+
+  private agregarAuthorizationHeader(): HttpHeaders {
+    const token = this.authService.token;
+    if (token != null) {
+      return this.httpHeaders.append('Authorization', 'Bearer ' + token);
+    }
+    return this.httpHeaders;
+  }
 
   private isNoAutorizado(e: any): boolean {
     if (e.status === 401 || e.status === 403) {
@@ -29,7 +38,7 @@ export class ClienteService {
   // como solo tenenmos el get de regiones no creo otro servicio aparte aplico la misma
   // logica que en backend
   public getRegiones(): Observable<Region[]> {
-    return this.http.get<Region[]>(`${this.baseUrl}/clientes/regiones`).pipe(
+    return this.http.get<Region[]>(`${this.baseUrl}/clientes/regiones`, {headers: this.agregarAuthorizationHeader()}).pipe(
       catchError(e => {
         this.isNoAutorizado(e);
         return throwError(e);
@@ -54,7 +63,7 @@ export class ClienteService {
   }
 
   public getCliente(id: number): Observable<Cliente> {
-    return this.http.get<Cliente>(`${this.baseUrl}/clientes/${id}`).pipe(
+    return this.http.get<Cliente>(`${this.baseUrl}/clientes/${id}`, {headers: this.agregarAuthorizationHeader()}).pipe(
       catchError(e => {
         if (this.isNoAutorizado(e)) {
           return throwError(e);
@@ -68,7 +77,7 @@ export class ClienteService {
   }
 
   public create(cliente: Cliente): Observable<ClienteResponse> {
-    return this.http.post<ClienteResponse>(`${this.baseUrl}/clientes`, cliente, {headers: this.httpHeaders}).pipe(
+    return this.http.post<ClienteResponse>(`${this.baseUrl}/clientes`, cliente, {headers: this.agregarAuthorizationHeader()}).pipe(
       catchError(e => {
         if (this.isNoAutorizado(e)) {
           return throwError(e);
@@ -85,7 +94,7 @@ export class ClienteService {
   }
 
   public update(cliente: Cliente): Observable<ClienteResponse> {
-    return this.http.put<ClienteResponse>(`${this.baseUrl}/clientes/${cliente.id}`, cliente, {headers: this.httpHeaders}).pipe(
+    return this.http.put<ClienteResponse>(`${this.baseUrl}/clientes/${cliente.id}`, cliente, {headers: this.agregarAuthorizationHeader()}).pipe(
       catchError(e => {
         if (this.isNoAutorizado(e)) {
           return throwError(e);
@@ -102,7 +111,7 @@ export class ClienteService {
   }
 
   public delete(id: number): Observable<ClienteResponse> {
-    return this.http.delete<ClienteResponse>(`${this.baseUrl}/clientes/${id}`, {headers: this.httpHeaders}).pipe(
+    return this.http.delete<ClienteResponse>(`${this.baseUrl}/clientes/${id}`, {headers: this.agregarAuthorizationHeader()}).pipe(
       catchError(e => {
         if (this.isNoAutorizado(e)) {
           return throwError(e);
@@ -118,8 +127,15 @@ export class ClienteService {
     const formData = new FormData();
     formData.append('archivo', archivo);
     formData.append('id', id);
+
+    let httpHeaders = new HttpHeaders();
+    const token = this.authService.token;
+    if (token != null) {
+      httpHeaders = httpHeaders.append('Authorization', 'Bearer ' + token);
+    }
     const req = new HttpRequest('POST', `${this.baseUrl}/clientes/upload`, formData, {
-      reportProgress: true
+      reportProgress: true,
+      headers: httpHeaders
     });
     return this.http.request(req).pipe(
       catchError(e => {
