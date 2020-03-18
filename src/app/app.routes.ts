e4b8820db58bf68b16049232529371c0aea27db6
israel <israel.bejarano.a@gmail.com>
2020-03-18 10:39:41 +0100

    role guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,14 +4,15 @@ import { ClientesComponent } from './components/clientes/clientes.component';
 import { FormComponent } from './components/clientes/form.component';
 import { LoginComponent } from './components/usuarios/login.component';
 import { AuthGuard } from './guards/auth.guard';
+import { RoleGuard } from './guards/role.guard';
 
 const appRoutes: Routes = [
     {path: '', redirectTo: '/clientes', pathMatch: 'full'},
     {path: 'directivas', component: DirectivaComponent},
     {path: 'clientes', component: ClientesComponent},
     {path: 'clientes/page/:page', component: ClientesComponent},
-    {path: 'clientes/form', component: FormComponent, canActivate: [AuthGuard]},
-    {path: 'clientes/form/:id', component: FormComponent, canActivate: [AuthGuard]},
+    {path: 'clientes/form', component: FormComponent, canActivate: [AuthGuard, RoleGuard], data: {role: 'ROLE_ADMIN'}},
+    {path: 'clientes/form/:id', component: FormComponent, canActivate: [AuthGuard, RoleGuard], data: {role: 'ROLE_ADMIN'}},
     {path: 'login', component: LoginComponent}
 ];
 
