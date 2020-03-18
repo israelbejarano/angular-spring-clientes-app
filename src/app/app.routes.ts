cc2e00c573a22530c5d320e59bb2ea649742f5cc
israel <israel.bejarano.a@gmail.com>
2020-03-18 10:29:46 +0100

    auth guard para proteger rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,14 +3,15 @@ import { DirectivaComponent } from './components/directiva/directiva.component';
 import { ClientesComponent } from './components/clientes/clientes.component';
 import { FormComponent } from './components/clientes/form.component';
 import { LoginComponent } from './components/usuarios/login.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const appRoutes: Routes = [
     {path: '', redirectTo: '/clientes', pathMatch: 'full'},
     {path: 'directivas', component: DirectivaComponent},
     {path: 'clientes', component: ClientesComponent},
     {path: 'clientes/page/:page', component: ClientesComponent},
-    {path: 'clientes/form', component: FormComponent},
-    {path: 'clientes/form/:id', component: FormComponent},
+    {path: 'clientes/form', component: FormComponent, canActivate: [AuthGuard]},
+    {path: 'clientes/form/:id', component: FormComponent, canActivate: [AuthGuard]},
     {path: 'login', component: LoginComponent}
 ];
 
