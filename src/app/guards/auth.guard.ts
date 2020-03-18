7dd5757126c2749516b7e87b54866d2a08374eeb
israel <israel.bejarano.a@gmail.com>
2020-03-18 10:49:29 +0100

    validando fecha expiracion token

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -14,10 +14,26 @@ export class AuthGuard implements CanActivate {
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
       if (this.authService.isAuthenticated()) {
+        if (this.isTokenExpirado()) {
+          this.authService.logout();
+          this.router.navigate(['/login']);
+          return false;
+        }
         return true;
       }
       this.router.navigate(['/login']);
       return false;
+  }
+
+  isTokenExpirado(): boolean {
+    const token = this.authService.token;
+    const payload = this.authService.obtenerDatosToken(token);
+    const now = new Date().getTime() / 1000;
+    if (payload.exp < now) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
 }
