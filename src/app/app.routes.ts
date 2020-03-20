61a845df664994adbb52aff8648cdf1469f47cc7
israel <israel.bejarano.a@gmail.com>
2020-03-20 10:49:04 +0100

    ruta para el detalle factura

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { DirectivaComponent } from './components/directiva/directiva.component';
 import { ClientesComponent } from './components/clientes/clientes.component';
 import { FormComponent } from './components/clientes/form.component';
 import { LoginComponent } from './components/usuarios/login.component';
+import { DetalleFacturaComponent } from './components/facturas/detalle-factura.component';
 import { AuthGuard } from './guards/auth.guard';
 import { RoleGuard } from './guards/role.guard';
 
@@ -13,7 +14,8 @@ const appRoutes: Routes = [
     {path: 'clientes/page/:page', component: ClientesComponent},
     {path: 'clientes/form', component: FormComponent, canActivate: [AuthGuard, RoleGuard], data: {role: 'ROLE_ADMIN'}},
     {path: 'clientes/form/:id', component: FormComponent, canActivate: [AuthGuard, RoleGuard], data: {role: 'ROLE_ADMIN'}},
-    {path: 'login', component: LoginComponent}
+    {path: 'login', component: LoginComponent},
+    {path: 'facturas/:id', component: DetalleFacturaComponent}
 ];
 
 
