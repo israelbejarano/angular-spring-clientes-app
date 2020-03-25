28e51a612174507dd3bf1eb639dc1a17dc0c7cf5
israel <israel.bejarano.a@gmail.com>
2020-03-25 11:15:17 +0100

    actualizar la cantidad de un item en la factura

--- src/app/components/facturas/facturas.component.ts
+++ src/app/components/facturas/facturas.component.ts
@@ -59,4 +59,14 @@ export class FacturasComponent implements OnInit {
     event.option.deselect();
   }
 
+  actualizarCantidad(id: number, event: any) {
+    const cantidad = event.target.value as number;
+    this.factura.items = this.factura.items.map((item: ItemFactura) => {
+      if (id === item.producto.id) {
+        item.cantidad = cantidad;
+      }
+      return item;
+    });
+  }
+
 }
